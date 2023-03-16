d0496e5a63ddbbcaa72042c387d3af80c7cfe0b8
El Mehdi Lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-16 16:21:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main entry point
+ * dipllay output using printf
+ * return 0 if succes
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return 0;
+}
