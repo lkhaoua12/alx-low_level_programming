000d798005e4503c58d0526c8c95507fa589e5b0
El Mehdi Lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-16 16:11:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 # include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Description: 'programm that print a string'
  * @parameter: describe the parameter
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/*funtcion puts that print*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
