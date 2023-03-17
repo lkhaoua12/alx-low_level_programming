8c6b4cac396d166d9ff2c7c4a6bee809473833c6
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-17 23:41:15 +0100

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n < '9'; n++){
+	putchar(n);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('9');
+
+}
