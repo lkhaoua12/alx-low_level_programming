fc82f4c9b9d0121450582dee91754855ca60416a
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-17 23:08:13 +0100

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 1;
+
+	for (n; n <= 9; n++)
+	printf("%d", n);
+
+	return(0);
+}
