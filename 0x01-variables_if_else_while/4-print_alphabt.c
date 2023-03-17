d7539f5c9e5936fd7c914d40ed4b7837907d23e0
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-17 22:56:19 +0100

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+	if (c == 'e' || c=='q')
+	c++;
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
