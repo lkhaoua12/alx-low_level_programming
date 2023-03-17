86af741ac612a5470dc99ca26580dc5a359f1a89
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-17 22:06:05 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+#include <stdio.h>
+
+int main()
+{
+	int alph;
+	
+	alph = 'a';
+	for( alph; alph != 'z'; alph++)
+	{
+	putchar(alph);
+	}
+	putchar("\n");
+	return 0;
+}
