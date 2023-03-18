ed77d8f8dfa0d31271c584c1e6e061a959df9541
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-18 01:03:12 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*
+*/
+
+int main(void)
+{
+	int n = 'z';
+
+	for (n = 'z'; n >= 'a' ; n--)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+
+}
+
