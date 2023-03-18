c0d54475713858a533cb5899648bbd331b72206f
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-18 01:38:05 +0100

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int n;
 
-	for (n = '0'; n < '9'; n++)
+	for (n = '48'; n < '57'; n++)
 	{
 	putchar(n);
 	putchar(',');
 	putchar(' ');
 	}
-	putchar('9');
+	putchar('/n');
 	return (0);
 
 }
