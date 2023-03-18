042b676036a8ef2cfcbb1280cc9bdf4a18842008
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-18 03:08:23 +0100

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - print all possible combinations of single-digit numbers
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+	putchar(c);
+	}
+	for (c = 97; c <= 102; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
