d347945f5c73035c8d740b455151320fc2645965
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-23 17:30:48 +0000

    print the line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @i: integer
+ * @j: integer
+ * Return: the result
+ */
+void print_line(int n)
+{
+	int lenght;
+
+	for (lenght = 1; lenght <= n; lenght++)
+	{	
+		if (n == 0)
+		break;
+		_putchar(95);
+	}
+	_putchar('\n');
+
+}
