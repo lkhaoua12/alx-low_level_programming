84ae443c60331ec89fdfa861baea458a9095360f
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-23 23:53:16 +0000

    print diagonaly

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0
+ */
+void print_diagonal(int n)
+{
+	int i;
+	
+	if (n == 0)
+   	_putchar('\n');
+	else
+	for (i = 1; i <= n; i++)
+	{
+		int j;
+
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(32);
+		}		
+		_putchar(92);
+		_putchar('\n');
+	}
+}
