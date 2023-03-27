3dec3380e3bff214f3dcebe1c19c8ca0c025951f
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-27 23:16:41 +0000

    0-sapw.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h" 
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i < *s; i++)
+	{
+		i++;
+	}
+	retourn (i); 
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@
 int _putchar(char c);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
+int _strlen(char *s);
 
 #endif
