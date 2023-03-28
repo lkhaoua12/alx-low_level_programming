2b7d2c0f93eb713a713b4d4cc4d8eaa4b86bd973
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-28 02:22:13 +0000

    revers string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h> 
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
+
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i]; = src[i];
+		i++
+	}
+	return (dest);
+}
