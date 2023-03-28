a0cd288f13c90a8b7b9b35ece3e591740fe1a3a8
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-28 01:50:28 +0000

    7puts

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h> 
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
+
+
+void puts_half(char *str)
+{
+	int lenght = strlen(str);
+	int middle = lenght / 2;
+
+	int i;
+	for (i = middle; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
