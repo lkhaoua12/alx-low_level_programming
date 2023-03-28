f965dfdbf709f381df1beaf056d1cebb2f81e826
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-28 04:36:00 +0000

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,28 @@
 #include "main.h"
 
 /**
- * print_triangle.
- * 
+ * rev_string - reverses a string
+ * @s: string to be reversed
  */
-
 void rev_string(char *s)
 {
-	int lenght = strlen(s);
-	int middle = lenght / 2;
-	
-	int i;
-	char temp;
+	char tmp;
+	int i, len, len1;
 
-	for (i = 0; i < middle; i++)
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
-		temp = s[i];
-		s[i] = s[lenght -i -1];
-		s[lenght - i -1] = temp;
+		len++;
 	}
 
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
