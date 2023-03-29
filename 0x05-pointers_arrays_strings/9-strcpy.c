6009a0aa2ee0202e919f14802dbe6e4577741f13
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-29 01:52:25 +0000

    9-print_array.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,18 @@
 #include "main.h"
 #include <string.h> 
 /**
- * main - Entry point
- * 
- * Description: 'the program's description'
- * @parameter: describe the parameter
- * 
- * Return: Always 0 (Success)
+ * *_strcpy - copie a string
+ * @dest: new string
+ * @src: string to copy
+ * Return: dest
  */
-
-
-
 char *_strcpy(char *dest, char *src)
 {
 	int i = -1;
+
 	do {
 		i++;
 		dest[i] = src[i];
 	} while (src[i] != '\0');
-
 	return (dest);
 }
