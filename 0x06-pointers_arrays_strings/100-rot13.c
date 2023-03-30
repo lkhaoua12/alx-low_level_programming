7e2b96f49173369c6d652ee80cf0d7c1d4e34c8d
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-30 18:30:56 +0000

    5 to upper

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *n)
+{
+	int i;
+	int j;
+	char line1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char line2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (n[i] == line1[j])
+			{
+				n[i] = line2[j];
+				break;
+			}
+		}
+	}
+	return (n);
+}
