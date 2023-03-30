0c426bcd089d7e2bbcdd6eb7df3c7a8eb38668f8
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-30 00:56:55 +0000

    3-strcomp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,9 @@
+#include "main.h"
 /**
  * _strcmp - concat two strings
  * @s1: first string
  * @s2: second string
- * Return: 0 if succes;
+ * Return: value;
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -13,8 +14,8 @@ int _strcmp(char *s1, char *s2)
 		if (*s1 != *s2)
 		{
 			value = ((int)*s1 - 48) - ((int)*s2 - 48); 
+			break;
 		}
-		break;
 		s1++;
 		s2++;
 	}
