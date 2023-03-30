8109ba340a38527e0ed7b78a4b9b8128ba8b25f3
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-30 18:31:48 +0000

    5 to upper

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * rot13 - encoder rot13
- * @s: pointer to string params
+ * @n: pointer to string params
  *
  * Return: *s
  */
-
 char *rot13(char *n)
 {
 	int i;
