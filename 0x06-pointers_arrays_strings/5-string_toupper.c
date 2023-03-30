ac9501603f7ffb7a44ad4770d632d9a2d731580d
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-03-30 01:56:16 +0000

    look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * _strcat - concat two strings
+ * @dest: first string
+ * @src: second string
+ * Return: dest
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	while (str[i])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
