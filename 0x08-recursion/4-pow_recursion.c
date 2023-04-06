213a7a167e88aa44b9c18efd00150da0d0dee7af
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-06 02:02:45 +0000

    return power of x

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _pow_recursion - return x to the power of y.
+ *
+ * @n: number
+ * @y: power
+ * Return: x.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1)
+	{
+		return x;
+	}
+	else
+	{
+		return x *= _pow_recursion(x , y - 1);
+	}
+}
