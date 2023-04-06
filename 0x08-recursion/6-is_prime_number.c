ad55720238e509b1074e60cc11f6091f0f995899
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-06 02:58:16 +0000

    check if n is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,12 +5,19 @@
  * Return: int
  */
 
-int prime_check(int n, int i);	
+int prime_check(int n, int i);
+
+/**
+ * prime_check - check if n is prime
+ * @n: int number to check
+ * @i: startin at 2
+ * Return: int
+ */
+
 int is_prime_number(int n)
 {
-	return(prime_check(n, 2));
+	return (prime_check(n, 2));
 }
-
 int prime_check(int n, int i)
 {
 	if (i >= n && n > 1)
@@ -22,4 +29,4 @@ int prime_check(int n, int i)
 		return (0);
 	}
 	return (prime_check(n, i + 1));
-}	
+}
