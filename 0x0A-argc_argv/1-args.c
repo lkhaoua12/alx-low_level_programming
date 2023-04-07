db2327b8d32c783d67a1d8ebea9b0c8e523468ce
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-07 23:44:13 +0000

    program that print num of arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point
+ * @argc: argument count.
+ * @argv: argument value.
+ * Return: success (0).
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
