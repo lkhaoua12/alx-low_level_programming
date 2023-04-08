4c2e3bc5afea7037fc42bc55cca2250b969302c1
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-08 22:50:03 +0000

    resul of addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,18 +8,18 @@
  * Return: success (0).
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int i, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < '0' || *argv[i] > '9')
+		if (**(argv + i) < '0' || **(argv + i) > '9')
 		{
 			printf("Error\n");
 			return (1);
 		}
-	sum += atoi(argv[i]);
+	sum += atoi(*(argv + i));
 	}
 	printf("%d\n", sum);
 	return (0);
