1a68863f0523f5eaf4ad705679b1aefc40b57fac
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-08 02:19:22 +0000

    return additi of argumet

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Main entry point
+ * @argc: argument count.
+ * @argv: argument value.
+ * Return: success (0).
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 2)
+	{
+		return (printf("0\n"));
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < '0' || *argv[i] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]); 
+	}
+	printf("%d\n", sum);
+	return (0);
+}
