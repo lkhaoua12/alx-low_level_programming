174584f98c4ecc680db34e3fa9f106d59ca79367
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-11 04:29:31 +0000

    returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -28,7 +28,11 @@ char *_strdup(char *str)
 {
 	int i;
 	char *p = (char *)malloc(get_lenght(str));
-
+	
+	if (!str)
+	{
+		printf("failed to allocate memory\n");
+	}
 	for (i = 0; i < get_lenght(str); i++)
 	{
 		*(p + i) = *(str + i);
