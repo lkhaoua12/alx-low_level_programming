8ac97b33b6f9f2b25fd8ad8eb253c639f9f5874c
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-11 04:27:46 +0000

    returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * get_lenght - get string lenght.
+ * @str: string to check it lenght.
+ * Return: int.
+ */
+
+int get_lenght(char *str)
+{
+	int lenght = 0;
+
+	while (*str)
+	{
+		lenght++;
+		str++;
+	}
+	return (lenght);
+}
+
+/**
+ * _strdup - copy a string to heap.
+ * @str: string to copy.
+ * Return: a pointer to string.
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *p = (char *)malloc(get_lenght(str));
+
+	for (i = 0; i < get_lenght(str); i++)
+	{
+		*(p + i) = *(str + i);
+	}
+	return (p);
+}
