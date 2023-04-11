0182f12cc2022a8b9029455540a47484b2e47c16
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-11 04:57:43 +0000

    concatenates two strings.

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_lenght - get string lenght.
+ * @str: string to check it lenght.
+ * Return: int.
+ */
+
+int get_lenght(char *str)
+{
+	int lenght = 0;
+
+	while (*str)
+	{
+		lenght++;
+		str++;
+	}
+	return (lenght);
+}
+
+/**
+ * str_concat - concat two string
+ * @s1: first string
+ * @s2: second string
+ * Return: a char pointer.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	char *p = (char *)malloc(get_lenght(s1) + get_lenght(s2));
+
+	for (i = 0; i < get_lenght(s1); i++)
+	{
+		*(p + i) = *(s1 + i);
+	}
+	for (i = 0; i < get_lenght(s2); i++)
+	{
+		*(p + get_lenght(s1) + i) = *(s2 + i);
+	}
+	return (p);
+}
