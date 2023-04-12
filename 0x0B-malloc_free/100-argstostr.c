be799d6afdba9f47ae4570cd537a245c4d5d3dc3
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-12 05:50:21 +0000

    pointer to pointer

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * get_lenght - get string lenght.
+ * @str: string to check it lenght.
+ * Return: int.
+ */
+
+int get_lenght(char *str)
+{
+	int lenght = 0;
+
+	while (*str)
+	{
+		lenght++;
+		str++;
+	}
+	return (lenght);
+}
+
+/**
+ * argstostr - return pointer to new string
+ * @ac: num of argument
+ * @av: pointer to pointer
+ * Return: char pointer.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+	int ocp = 0;
+	int k = 0;
+	char *p;
+
+	if (ac == 0 || !av)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++, ocp++)
+	{
+		ocp += get_lenght(*(av + i)); 
+	}
+	p = (char *)malloc(sizeof(char) * ocp + 1);
+	if (!p)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j); j++)
+		{
+			*(p + k) = *(*(av + i) + j);
+			k++;
+		}
+		*(p + k) = '\n';
+		k++;
+	}
+	return (p);
+}
