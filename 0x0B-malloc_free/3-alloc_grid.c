a7e6072567e2f937254c86b1415efa5f0b8f6472
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-12 00:21:44 +0000

    pointer to pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,18 +1,24 @@
 #include "main.h"
 /**
- *
- *
- *
+ * alloc_grid - allcoate a 2d array
+ * @width: the number of colums
+ * @height: number of rows
+ * Return: pointer to pointer
  */
 
 int **alloc_grid(int width, int height)
 {
-	int i, j;
-	int (*p)[height] = (int **)malloc(height * width);
+	int **p, i, j;
 
-	for (i = 0; i < height - 1; i++)
+	p = (int **)malloc(sizeof(int *) * height);
+
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j < width - 1; j++)
+		*(p + i) = (int *)malloc(sizeof(int) * width);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
 		{
 			*(*(p + i) + j) = 0;
 		}
