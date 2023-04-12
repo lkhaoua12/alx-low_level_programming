2b1fe269f65ca65769ae5440b24509e6d392662f
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-12 01:30:34 +0000

    pointer to pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 		*(p + i) = (int *)malloc(sizeof(int) * width);
 		if (*(p + i) == 0)
 		{
-			for (; i <= 0; i--)
+			while (i--)
 			{
 				free(*(p + i));
 			}
