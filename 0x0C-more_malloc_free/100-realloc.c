f2eba8fb554fe5fea4888901c9e0777fed44f898
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-15 02:52:39 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
 	void *newPtr;
-	
+
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr)
