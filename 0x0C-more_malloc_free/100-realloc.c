daf09f1c7d619eefe856d1860d5b346bd4a527b2
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-15 02:49:19 +0000

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,6 +35,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			*((char *)newPtr + i) = *((char *)ptr + i);
 		}
 		free(ptr);
-		return (newPtr);
 	}
+	return (newPtr);
 }
