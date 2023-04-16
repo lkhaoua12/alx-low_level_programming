2a94647b7fb1449951d3f658ff022a9314bcf174
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-16 02:53:34 +0000

    mul func

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,12 +8,10 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-/**
 int _putchar(char c)
 {
         return (write(1, &c, 1));
 }
-*/
 /**
  * _puts - prints an integer to stdout
  * @num: The integer to print
