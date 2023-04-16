4a190e95f1ea7ab30c6869372a82b8d91844f7b3
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-16 03:03:22 +0000

    mul func

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,7 +4,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
-/* int _putchar(char c); */
+#include <unistd.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
