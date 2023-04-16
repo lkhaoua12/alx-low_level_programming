15bdbf71b95130e5171c0f4c95cb53b1b3124ff4
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-16 03:12:34 +0000

    hello

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+int _putchar(char c);
+void _puts(int num);
+int my_isspace(char c);
+long _atoi(char *num);
+void _otherputs(char *exitt);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
