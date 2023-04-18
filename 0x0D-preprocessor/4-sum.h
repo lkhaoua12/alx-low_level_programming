2da00356acfb2e5979d74f6129dbbacd20e0b6c2
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-18 01:36:16 +0000

    macro pi

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,16 @@
+#ifndef AHEADER_FILE
+#define AHEADER_FILE
+
+/**                                                          
+ * ABS - Computes the absolute value of a number x.
+ * @x: The number to compute the absolute value of.
+ *
+ * Return: The absolute value of x.
+ */
+
+#define SUM(x, y) ((x) * (y))
+
+
+#endif
+
+
