765d4cc651ee00f0d384ae53350a8ed73bc06206
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-18 01:41:38 +0000

    macro pi

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,11 +1,10 @@
 #ifndef CHEADER_FILE
 #define CHEADER_FILE
 
-/**                                                          
- * SUM - Computes the sum of x and y.
+/**
+ * SUM - Computes the sum of x and y
  * @x: First num of sum.
  * @y: Second num of sum.
- *
  * Return: The sum of x and y.
  */
 
