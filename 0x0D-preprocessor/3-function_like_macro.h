a01b91b06cc47a94ba8ac2a93caa8e55406e7f93
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-18 01:33:12 +0000

    macro pi

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,15 @@
+#ifndef 2HEADER_FILE
+#define 2HEADER_FILE
+
+/**
+ * ABS - Computes the absolute value of a number x.
+ * @x: The number to compute the absolute value of.
+ *
+ * Return: The absolute value of x.
+ */
+
+#define ABS(x) ((x > 0) ? (x) : -(x))
+
+
+#endif
+
