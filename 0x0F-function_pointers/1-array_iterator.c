f35928a07d4dea1479d8e6a6aa7a6f36952e9a91
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-20 02:22:49 +0000

    pointer to array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,10 @@
 #include "function_pointers.h"
 /**
- * array_iterator - Iterates over an integer array and applies the action function to each element
+ * array_iterator - Iterates over an integer array
  *
  * @array: A pointer to the first element of the integer array
  * @size: The size of the array
- * @action: A function pointer 
+ * @action: A function pointer
  *
  * Return: void
  */
@@ -13,6 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array && size && action)
 	{
 		int i, *last_element;
+
 		last_element = array + size - 1;
 		for (i = 0; (array + i) <= last_element; i++)
 			action(*(array + i));
