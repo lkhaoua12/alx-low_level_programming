7f9e21a130e3c72b290d7e737d4022161a83cfb5
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-20 23:12:14 +0000

    calc fun

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,17 +1,18 @@
 #ifndef HEADER
 #define HEADER
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct op - Struct op
  *
  * @op: The operator
  * @f: The function associated
  */
-#include <stdio.h>
-#include <stdlib.h>
+
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
