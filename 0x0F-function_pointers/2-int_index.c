8ff1ebc5bcba6578e1afe8c1c1fab241640b4295
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-20 02:53:34 +0000

    find index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (array && cmp && size > 0)
 	{
-		for (i = 0; i < size - 1; i++)
+		for (i = 0; i < size; i++)
 		{
 			if (cmp(*(array + i)))
 				return (i);
