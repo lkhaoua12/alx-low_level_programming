b8602cafc74da216af80e2162d9e8847c9bf4360
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-21 02:19:25 +0000

    add num

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,12 @@
 #include "variadic_functions.h"
-
+/**
+ * sum_them_all - returns the sum of all its parameters
+ *
+ * @n: number of integers passed as arguments
+ * ...: variable number of integers to be summed
+ *
+ * Return: sum of all integers, or 0 if n is 0
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	int result = 0, i = n;
