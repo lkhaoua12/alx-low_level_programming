668dbd342b26e742bbd494ab9a1c51ebf923d266
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-21 01:47:00 +0000

    add num

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list p;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(p);
+	for (i = n; i > 1; i--)
+	{
+		printf("%d%s", va_arg(p, int), separator ? separator : "");
+	}
+	printf("%d\n" va_arg(p, int));
+	va_end(p);
+}
