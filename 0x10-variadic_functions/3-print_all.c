69e682e11b3be9b10c9813548e4a831713eca3d4
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-21 03:29:39 +0000

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,30 +1,51 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-
+/**
+ * get_string - Prints a string argument.
+ * @seperator: The string separator to use.
+ * @p: The va_list containing the argument to print.
+ */
 void get_string(char *seperator, va_list p)
 {
 	char *stri = va_arg(p, char *);
+
 	if (!stri)
 	stri = "(nil)";
 	printf("%s%s", seperator, stri);
 }
-
+/**
+ * get_float - Prints a float argument.
+ * @seperator: The string separator to use.
+ * @p: The va_list containing the argument to print.
+ */
 void get_float(char *seperator, va_list p)
 {
 	printf("%s%f", seperator, va_arg(p, double));
 }
-
+/**
+ * get_char - Prints a char argument.
+ * @seperator: The string separator to use.
+ * @p: The va_list containing the argument to print.
+ */
 void get_char(char *seperator, va_list p)
 {
 	printf("%s%c", seperator, va_arg(p, int));
 }
-
+/**
+ * get_int - Prints an int argument.
+ * @seperator: The string separator to use.
+ * @p: The va_list containing the argument to print.
+ */
 void get_int(char *seperator, va_list p)
 {
 	printf("%s%d", seperator, va_arg(p, int));
 }
-
+/**
+ * print_all - Prints a list of arguments of different types.
+ * @format: A format string containing the type of each argument.
+ * @...: A variable number of arguments to print.
+ */
 void print_all(const char * const format, ...)
 {
 	int i = 0, j;
@@ -46,9 +67,9 @@ void print_all(const char * const format, ...)
 	while (print[j].print)
 	{
 		if (print[j].print == format[i])
-            	{
-                	print[j].f(seperator, p);
-                	seperator = ", ";
+		{
+			print[j].f(seperator, p);
+			seperator = ", ";
 			break;
 		}
 		j++;
