77c57ae6c225ddb3f56b81280a82d150a2efad69
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-21 02:18:22 +0000

    add num

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,13 @@
 #include "variadic_functions.h"
-
-
+/**
+ * print_numbers - prints numbers with separator
+ *
+ * @separator: string to be printed between numbers
+ * @n: number of numbers passed as arguments
+ * ...: variable number of integers to be printed
+ *
+ * Return: void
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	int i;
