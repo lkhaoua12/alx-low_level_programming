f03d082f62c1512c3ed63000edc20bb7945a1673
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-21 01:33:34 +0000

    add num

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,18 @@
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int result = 0; i = n;
+	va_list ap;
+
+	if (!n)
+		return (0);
+	va_start(ap, n);
+	while (i--)
+	{
+		result = result + va_arg(ap, int);
+	}
+	va_end(ap);
+	return (result);
+
+}
