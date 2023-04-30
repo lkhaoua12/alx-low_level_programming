34dd729f2a8a87019103a99b0bdcf585d09287d3
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-30 00:34:40 +0000

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list and sets the head to NULL
+ * @head: pointer to pointer to the head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+
+	if (head == NULL)
+		return;
+
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
+
