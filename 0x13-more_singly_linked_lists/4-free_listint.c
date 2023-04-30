89cd2abc04d7b7dc8807e8cadcef82c0797aa220
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-04-30 00:20:09 +0000

    free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint - frees a linked list
+ * @head: pointer to the head of the linked list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (tmp)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
+
