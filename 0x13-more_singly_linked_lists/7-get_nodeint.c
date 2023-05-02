1bafacbac3af378fd94f4b7be35436a62bfd0fbb
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-03 00:13:58 +0100

    return nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,10 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *tmp = head;
-
+	if (!head)
+	{
+		return (NULL);
+	}
 	while (--index)
 	{
 		tmp = tmp->next;
