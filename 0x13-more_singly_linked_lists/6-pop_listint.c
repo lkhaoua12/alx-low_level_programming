5cc19b554610e23b1eb8754499931ba8a43f875a
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-03 00:00:16 +0100

    pop first node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * pop_listint - removes the first element of a linked list
+ * @head: pointer to pointer to the first node of the linked list
+ *
+ * Return: the value of the removed node, or -1 if the linked list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *new_head, *tmp;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	tmp = *head;
+	n = (*head)->n;
+	new_head = (*head)->next;
+	*head = new_head;
+	free(tmp);
+	return (n);
+}
+
