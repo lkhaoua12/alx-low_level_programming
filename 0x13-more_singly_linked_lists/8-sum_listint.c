973ef4dfa0cfc6e69fbd4b8a4157189b5a423068
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-03 01:32:06 +0100

    return sum of node

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * sum_listint - a function that handle a linked list.
+ * @head: pointer to the first node.
+ * Return: the sum of the nodes
+ */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
