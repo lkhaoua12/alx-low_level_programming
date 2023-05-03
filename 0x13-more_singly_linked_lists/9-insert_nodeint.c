ff0d307ae2c36e6fa7f40bbfa006f66a90a54b16
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-03 07:29:43 +0100

    insert at nth

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,9 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	if (head == NULL)
-	{
-		return NULL;
-	}
+		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
