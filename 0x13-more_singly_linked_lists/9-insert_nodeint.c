b1ff60fbc53330764c020c709c03bab3eba218fa
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-03 02:36:57 +0100

    insert at nth

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,11 +23,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
+	if (*head == NULL && idx == 0)
+	{
+		new_node->next = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+
 	while (tmp && i < idx - 1)
 	{
 		tmp = tmp->next;
 		i++;
 	}
+
 	if (!tmp)
 	{
 		free(new_node);
