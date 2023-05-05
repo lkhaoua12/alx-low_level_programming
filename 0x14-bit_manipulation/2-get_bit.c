90284e2c58858dfd907e4ecf486347d126fc11f4
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-05 02:18:32 +0100

    binary to usngined int

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * get_bit - returns the value of the bit at a specified index in a given number
+ *
+ * @n: the number to extract the bit from
+ * @index: the index of the bit to extract (0 is the least significant bit)
+ *
+ * Return: the value of the bit at the specified index, or -1 if index is out of range
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i = 1UL;
+
+	if (!n)
+		return (-1);
+
+	i <<= index;
+
+	if (n & i)
+		return (1);
+	else
+		return (0);
+}
