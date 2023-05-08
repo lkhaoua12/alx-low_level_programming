84a394dbcaab870d98040323bff223637e72003e
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-09 00:44:24 +0100

    creat and write to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_WRONLY, mode);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
 
 	if (fd == -1)
 	{
