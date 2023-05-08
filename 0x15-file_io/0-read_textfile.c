a1be5e8c8a159a17a692c6030c8137f83d8aeebd
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-08 23:40:16 +0100

    read from file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,25 +8,25 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes_written; 
+	ssize_t bytes_written;
 	size_t bytes_read;
 	size_t fp;
 	char *buffer;
 
 	if (filename == NULL)
-		return 0;
+		return (0);
 
 	fp = open(filename, O_RDONLY);
 
 	if ((ssize_t)fp == -1)
-		return 0;
+		return (0);
 
 	buffer = malloc(sizeof(char) * (letters + 1));
 
 	if (buffer == NULL)
 	{
 		close(fp);
-		return 0;
+		return (0);
 	}
 
 	bytes_read = read(fp, buffer, letters);
@@ -34,7 +34,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		close(fp);
 		free(buffer);
-		return 0;
+		return (0);
 	}
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
@@ -43,7 +43,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		close(fp);
 		free(buffer);
-		return 0;
+		return (0);
 	}
 	close(fp);
 	free(buffer);
