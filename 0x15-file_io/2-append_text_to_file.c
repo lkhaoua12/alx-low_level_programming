4bd116b35485d32a200ce09a77e0b09718b2c9ee
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-09 01:01:15 +0100

    creat and write to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,18 +11,23 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	if(text_content == NULL)
+	if (text_content == NULL)
+	{
+		close(fd);
 		return (1);
+	}
 
 	bytes_written = write(fd, text_content, strlen(text_content));
 
 	if (bytes_written == -1)
+	{
+		close(fd);
 		return (-1);
-
+	}
 	return (1);
 }
