a2bb64edfc0d702b12c980f2d7dca28f404bccbe
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-09 01:02:59 +0100

    creat and write to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * append_text_to_file - append text to an already existing file.
+ * @filename: path to the file.
+ * @text_content: content to be appended.
+ * Return: 1 if succes, -1 otherwise
+ */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
