02574f09e151249ef73ca85f398f73808e22eeaa
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-05-09 02:28:24 +0100

    cp from file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,12 +23,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	wd = open(argv[2], O_RDONLY | O_WRONLY | O_CREAT, FILE_PERMISSIONS);
-	if (wd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(100);
-	}
+	wd = open(argv[2], O_RDONLY | O_WRONLY | O_CREAT O_TRUNC, FILE_PERMISSIONS);
 	do {
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
@@ -40,10 +35,19 @@ int main(int argc, char **argv)
 		if (bytes_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(100);
+			exit(99);
 		}
 	} while (bytes_read > 0);
-
-
+	
+	if (close(input_fd) == -1)
+	{
+		fprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+		exit(100);
+	}
+	if (close(wd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", wd);
+		exit(100);
+	}
 	return (0);
 }
