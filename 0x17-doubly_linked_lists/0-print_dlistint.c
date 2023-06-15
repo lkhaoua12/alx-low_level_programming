64b04bfec1dbe0bdfc7b259cfa172e1fd2ce0641
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-15 23:33:18 +0100

    print elem of dllist

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t numberOfNode = 0;
+
+	if (h == NULL)
+		return (0);
+	while (temp != NULL)
+	{
+		numberOfNode += 1; 
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	return (numberOfNode);
+}
