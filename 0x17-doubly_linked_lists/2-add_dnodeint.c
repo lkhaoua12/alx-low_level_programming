3bfd9e716167e1f0e359dcccff1435f61719ff9f
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-16 00:00:11 +0100

    print elem of dllist

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,23 @@
 #include "lists.h"
-
+/**
+ * add_dnodeint - Adds a new node.
+ * @head: Pointer to a pointer to the head node.
+ * @n: Value to be stored in the new node.
+ *
+ * Return: Address of the new node.
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
+	
+	if (!n || new_node == NULL)
+	{
+		return (NULL);
+	}
 	new_node->n = n;
 	new_node->next = *head;
+	new_node->prev = NULL;
+
 	if (*head != NULL)
 	{
 		(*head)->prev = new_node;
