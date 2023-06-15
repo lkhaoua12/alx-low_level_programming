ba77533e5e3aacd4c7a0ac2f919e922dec589ebb
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-16 00:10:05 +0100

    print elem of dllist

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	
-	if (!n || new_node == NULL)
+	if (!head || new_node == NULL)
 	{
 		return (NULL);
 	}
