0a8dda88d83455e73d87c9477f34dfaa25d526d9
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-15 23:39:56 +0100

    print elem of dllist

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
  * print_dlistint - print all nodes of a dlinked_list.
- * h: point to the head of the list.
- * return: number of nodes
+ * @h: point to the head of the list.
+ * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
