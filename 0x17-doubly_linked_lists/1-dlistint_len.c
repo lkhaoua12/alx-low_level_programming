5e4c84de5d0269ad00ba653c58c2d90462674ca5
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-15 23:42:55 +0100

    print elem of dllist

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * dlistint_len - return number of nodes of a dlinked_list.
+ * @h: point to the head of the list.
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t numberOfNode = 0;
+
+	if (h == NULL)
+		return (0);
+	while (temp != NULL)
+	{
+		numberOfNode += 1;
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	return (numberOfNode);
+}
