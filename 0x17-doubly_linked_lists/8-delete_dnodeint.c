8da29c3a9106526e3a1ca7047e14a6f9917d450a
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-16 03:05:45 +0100

    print elem of dllist

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	unsigned i;
+
+	for (i = 0; i < index && temp != NULL; i++)
+	{
+		temp = temp->next;
+	}
+	if (temp == NULL)
+	{
+		return (-1);
+	}
+	
+	if (temp = *head)
+	{
+		*head = (*head)->next
+		if (head->next != NULL)
+			head->prev = NULL;
+		free(temp);
+		return (1);
+	}
+	temp->prev->next = temp->next;
+	if (temp->next != NULL)
+	{
+		temp->next->prev = temp->prev;
+	}
+	free(temp);
+	return (1);
+}
