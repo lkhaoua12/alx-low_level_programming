5ff0eed6baaa2b38d3eb2898ae4942c75a869933
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-16 03:10:47 +0100

    delete at idx

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -5,19 +5,23 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *temp = *head;
 	unsigned i;
 
+	if (head == NULL)
+	{
+		return (-1);
+	}
 	for (i = 0; i < index && temp != NULL; i++)
 	{
 		temp = temp->next;
 	}
-	if (temp == NULL)
+	if (temp == NULL || head == NULL)
 	{
 		return (-1);
 	}
 	
-	if (temp = *head)
+	if (temp == *head)
 	{
-		*head = (*head)->next
-		if ((*head)->next != NULL)
+		*head = (*head)->next;
+		if (*head != NULL)
 			(*head)->prev = NULL;
 		free(temp);
 		return (1);
