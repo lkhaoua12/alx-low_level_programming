fa294d12243915c1428866e6de3407a77dcea9fc
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-16 02:37:07 +0100

    print elem of dllist

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,10 +1,20 @@
 #include "lists.h"
+/**
+ * get_dnodeint_at_index - return node at index.
+ * @head: the first node in the linked list.
+ * @index: index to return.
+ * Return: pointer to node in the linked list.
+ */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 	dlistint_t *temp = head;
 
+	if (index < 0)
+	{
+		return (NULL);
+	}
 	for (i = 0; temp != NULL && i < index; i++)
 	{
 		temp = temp->next;
