f60fd97b043e0d0aa12d8ac352e30dbe6ec417bf
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-06-16 02:05:04 +0100

    print elem of dllist

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,27 +1,36 @@
 #include "lists.h"
-
+/**
+ * add_dnodeint_end - Adds a new node.
+ * @head: Pointer to the head of the list.
+ * @n: Value to be assigned to the new node.
+ *
+ * Return: Address of the new node, or NULL on failure.
+ */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *temp = *head;
+	dlistint_t *temp;
 
-	if (!head)
+	if (!new_node || !head)
 		return (NULL);
 
 	new_node->n = n;
 	new_node->next = NULL;
-	new_node->prev = NULL;
 
 	if (*head == NULL)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
-		return (*head);
+		return (new_node);
 	}
+
+	temp = *head;
 	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
+
 	temp->next = new_node;
 	new_node->prev = temp;
 
