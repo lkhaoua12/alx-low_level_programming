c8204df2f9548794baec3199912567467e74f2a1
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-08-24 03:21:26 +0100

    1-make file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """a function that find an island in a grid"""
 
+
 def island_perimeter(grid):
     """find insland in a given grid"""
 
@@ -12,7 +13,7 @@ def island_perimeter(grid):
         for j in range(column):
             if grid[i][j] == 1:
                 delimiter += 4
-                if i > 0 and grid[i-1][j] == 1:
+                if i > 0 and grid[i - 1][j] == 1:
                     delimiter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     delimiter -= 2
